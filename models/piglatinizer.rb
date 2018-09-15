fa69b978172badb1da1f981324d7ef55bee9c4f0
Nathan  Visak <nathan.visak@gmail.com>
2018-09-15 17:18:28 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,16 +4,22 @@ class PigLatinizer
 
 
   def piglatinize(string)
-    array = string.split("")
-      if array[0] =~ /[aeiouAEIOU]/
-        string + "way"
-      else
-        first_vowel = array.find_index { |e| e.match( /[aeiouAEIOU]/ ) } #returns the index of the first vowel
-        first_chunk = array[0...first_vowel].join("") #This first_chunk will be a consonant cluster
-        last_chunk = string.slice(first_vowel..-1)
-        last_chunk + first_chunk + "a" + "y"
-      end
-    end
+    piglatinize_word(string)
+      array = string.split(" ")
+  end
+
+    def piglatinize_word(word)
+      array = word.split("")
+        if array[0] =~ /[aeiouAEIOU]/
+          word + "way"
+        else
+          first_vowel = array.find_index { |e| e.match( /[aeiouAEIOU]/ ) } #returns the index of the first vowel
+          first_chunk = array[0...first_vowel].join("") #This first_chunk will be a consonant cluster
+          last_chunk = word.slice(first_vowel..-1)
+          last_chunk + first_chunk + "a" + "y"
+        end
+    end 
+      
 
 
 end #ends the class
