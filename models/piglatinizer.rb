abfa7edca1d3bd6e8b368c5a3c230996e34df2b9
Nathan  Visak <nathan.visak@gmail.com>
2018-09-15 16:29:14 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,32 +4,24 @@ class PigLatinizer
 
 
   def piglatinize(string)
-    array = string.split(" ")
-    if array.count == 1
-      isolated_first_element = array[0]
-        if array[0] =~ /[aioAIO]/
-          #this won't work unless you further split the array into its component string parts
-          array.shift
-          array << isolated_first_element
-          array << "w"
-          array << "a"
-          array << "y"
-          array.join
-        elsif array[0] =~ /[uU]/
-          string + "way"
-        elsif array[0] =~ /[eE]/
-    #elsif string.slice 
-          string + "way"
-        else
-          first_vowel = array.find_index { |e| e.match( /[aeiouAEIOU]/ ) } #returns the index of the first vowel
-          first_chunk = array[0...first_vowel].join("")
-          last_chunk = string.slice(first_vowel..-1)
-          last_chunk + first_chunk + "a" + "y"
-        end
-      end 
-    else 
-      array[0..-1].slice
+    array = string.split("")
+    isolated_first_element = array[0]
+      if array[0] =~ /[aioAIO]/
+        array.shift
+        array << isolated_first_element
+        array << "w"
+        array << "a"
+        array << "y"
+        array.join
+      elsif array[0] =~ /[uU]/
+        string + "way"
+      elsif array[0] =~ /[eE]/
+        string + "way"
+      else
+        first_vowel = array.find_index { |e| e.match( /[aeiouAEIOU]/ ) } #returns the index of the first vowel
+        first_chunk = array[0...first_vowel].join("")
+        last_chunk = string.slice(first_vowel..-1)
+        last_chunk + first_chunk + "a" + "y"
+      end
     end
 
-
-end
